8dc0c0831984981ab6a46200c6f3ceac3b5d3366
Vitor Régis <vitorregisrr@gmail.com>
2018-06-21 08:46:59 -0300

    chao mudado para variaveis diferentes

--- js/platformsGen.js
+++ js/platformsGen.js
@@ -3,10 +3,13 @@
     plataformas = game.add.group();
     plataformas.enableBody = true;
 
-    chao = plataformas.create(230,game.world.height - 123, 'chao1');
-    chao = plataformas.create(30,game.world.height - 140, 'chao2');
-    chao = plataformas.create(410,game.world.height - 160, 'chao2');
-    chao = plataformas.create(590,game.world.height - 180, 'chao3');
-    chao.body.immovable = true;
+    chao1 = plataformas.create(230,game.world.height - 123, 'chao1');
+    chao2 = plataformas.create(30,game.world.height - 140, 'chao2');
+    chao3 = plataformas.create(410,game.world.height - 160, 'chao2');
+    chao4 = plataformas.create(590,game.world.height - 180, 'chao3');
+    chao1.body.immovable = true;
+    chao2.body.immovable = true;
+    chao3.body.immovable = true;
+    chao4.body.immovable = true;
 }
  /* //PLATAFORMAS */
